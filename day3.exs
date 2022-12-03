43c2e67ffd55bd54ecb88aa25646f1bc8373d5b4
Thomas Cowart <tncowart@gmail.com>
2022-12-03 12:09:34 -0500

    AoC day 3 refactor

--- day3.exs
+++ day3.exs
@@ -1,31 +1,29 @@
 defmodule Day3 do
   @input File.stream!("day3.input", [encoding: :utf8], :line)
          |> Enum.map(&(String.trim(&1)))
+         |> Enum.map(&(String.to_charlist(&1)))
          |> Enum.into([])
 
   def part1() do
     @input
-    |> Enum.map(&(String.split_at(&1, div(String.length(&1), 2) + rem(String.length(&1), 2))))
-    |> Enum.map(fn {x, y} -> {MapSet.new(String.to_charlist(x)), MapSet.new(String.to_charlist(y))} end)
+    |> Enum.map(&(Enum.split(&1, div(length(&1), 2) + rem(length(&1), 2))))
+    |> Enum.map(fn {x, y} -> {MapSet.new(x), MapSet.new(y)} end)
     |> Enum.map(fn {x, y} -> MapSet.intersection(x, y) end)
-    |> Enum.map(&(MapSet.to_list(&1)))
-    |> Enum.map(&(List.first(&1)))
-    |> Enum.map(&(if &1 < 91 do
-      &1 - 38
-    else
-      &1 - 96
-    end))
-    |> Enum.sum()
+    |> priority_sum()
   end
 
   def part2() do
     @input
-    |> Enum.map(&(String.to_charlist(&1)))
     |> Enum.map(&(MapSet.new(&1)))
     |> Enum.chunk_every(3)
     |> Enum.map(fn [a, b, c] ->
       MapSet.intersection(MapSet.intersection(a, b), c)
     end)
+    |> priority_sum()
+  end
+
+  defp priority_sum(x) do
+    x
     |> Enum.map(&(MapSet.to_list(&1)))
     |> Enum.map(&(List.first(&1)))
     |> Enum.map(&(if &1 < 91 do
