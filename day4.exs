dc6925183bb5206df731a69295a9c2313755e4c0
Thomas Cowart <tncowart@gmail.com>
2022-12-04 21:16:35 -0500

    AoC day 4

---
+++ day4.exs
@@ -0,0 +1,33 @@
+defmodule Day4 do
+  @input File.stream!("day4.input", [encoding: :utf8], :line)
+         |> Stream.map(&tl(Regex.run(~r/(\d+)-(\d+),(\d+)-(\d+)/, &1)))
+         |> Stream.map(&Enum.map(&1, fn x -> String.to_integer(x) end))
+         |> Enum.into([])
+
+  def part1() do
+    @input
+    |> Enum.map(fn [a, b, x, y] ->
+      case Enum.sort([a, b, x, y]) do
+        [^a, ^x, ^y, ^b] -> 1
+        [^x, ^a, ^b, ^y] -> 1
+        _ -> 0
+      end
+    end)
+    |> Enum.sum()
+  end
+
+  def part2() do
+    @input
+    |> Enum.map(fn [a, b, x, y] ->
+      case Enum.sort([a, b, x, y]) do
+        [^a, ^b, ^x, ^y] when b != x -> 0
+        [^x, ^y, ^a, ^b] when y != a -> 0
+        _ -> 1
+      end
+    end)
+    |> Enum.sum()
+  end
+end
+
+Day4.part1() |> IO.puts()
+Day4.part2() |> IO.puts()
