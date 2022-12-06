b47ec7165a39042b35a5ae06044a7fc975690ecc
Thomas Cowart <tncowart@gmail.com>
2022-12-06 00:24:33 -0500

    Day 6 refactoring
    de-dupes parts 1 and 2 code. They are the same but with different numbers

--- day6.exs
+++ day6.exs
@@ -1,36 +1,24 @@
 defmodule Day6 do
-  @input File.open!("day6.input", [encoding: :utf8])
+  @input File.open!("day6.input", encoding: :utf8)
          |> IO.read(:eof)
          |> String.to_charlist()
 
-
-
-  def part1() do
+  def find_marker(size) do
     @input
-    |> Enum.chunk_every(4, 1)
-    |> Enum.with_index(4)
+    |> Enum.chunk_every(size, 1)
+    |> Enum.with_index(size)
     |> Enum.drop_while(fn {x, _} ->
-      4 != Enum.sort(x)
-      |> Enum.dedup()
-      |> length()
+      size !=
+        Enum.sort(x)
+        |> Enum.dedup()
+        |> length()
     end)
     |> List.first()
     |> elem(1)
   end
 
-  def part2() do
-    @input
-    |> Enum.chunk_every(14, 1)
-    |> Enum.with_index(14)
-    |> Enum.drop_while(fn {x, _} ->
-      14 != Enum.sort(x)
-      |> Enum.dedup()
-      |> length()
-    end)
-    |> List.first()
-    |> elem(1)
-  end
-
+  def part1(), do: find_marker(4)
+  def part2(), do: find_marker(14)
 end
 
 Day6.part1() |> IO.puts()
