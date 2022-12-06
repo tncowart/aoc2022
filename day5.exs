266fe4b8df5a9937d8f8a8594e2b97c6ce2d8512
Thomas Cowart <tncowart@gmail.com>
2022-12-05 22:28:31 -0500

    AoC Day 5 refactor
    part1 and part2 are almost the same, just changes the code a little
    to minimize the duplication
    
    Also replaces some Stream with Enum so Enum.to_list() isn't necessary

--- day5.exs
+++ day5.exs
@@ -1,9 +1,8 @@
 defmodule Day5 do
   @boxes File.stream!("day5.input", [encoding: :utf8], :line)
-         |> Stream.take(8)
-         |> Stream.map(&tl(String.to_charlist(&1)))
-         |> Stream.map(&Enum.take_every(&1, 4))
-         |> Enum.to_list()
+         |> Enum.take(8)
+         |> Enum.map(&tl(String.to_charlist(&1)))
+         |> Enum.map(&Enum.take_every(&1, 4))
          |> Enum.reduce(
            [[], [], [], [], [], [], [], [], []],
            &Enum.zip_with(&1, &2, fn x, a -> if x == 32, do: a, else: [x | a] end)
@@ -11,30 +10,22 @@ defmodule Day5 do
          |> Enum.map(&Enum.reverse/1)
 
   @moves File.stream!("day5.input", [encoding: :utf8], :line)
-         |> Stream.drop(10)
-         |> Stream.map(&tl(Regex.run(~r/move (\d+) from (\d+) to (\d+)/, &1)))
-         |> Stream.map(fn [x, y, z] -> [String.to_integer(x), String.to_integer(y) - 1, String.to_integer(z) - 1] end)
-         |> Enum.to_list()
+         |> Enum.drop(10)
+         |> Enum.map(&tl(Regex.run(~r/move (\d+) from (\d+) to (\d+)/, &1)))
+         |> Enum.map(fn [x, y, z] -> [String.to_integer(x), String.to_integer(y) - 1, String.to_integer(z) - 1] end)
 
-  def part1() do
+  def move_boxes(how) do
     @moves
     |> Enum.reduce(@boxes, fn [ct, from, to], a ->
       {top, new_from_list} = Enum.split(Enum.at(a, from), ct)
       x = List.replace_at(a, from, new_from_list)
-      List.replace_at(x, to, Enum.reverse(top) ++ Enum.at(a, to))
+      List.replace_at(x, to, how.(top) ++ Enum.at(a, to))
     end)
     |> Enum.map(&List.first/1)
   end
 
-  def part2() do
-    @moves
-    |> Enum.reduce(@boxes, fn [ct, from, to], a ->
-      {top, new_from_list} = Enum.split(Enum.at(a, from), ct)
-      x = List.replace_at(a, from, new_from_list)
-      List.replace_at(x, to, top ++ Enum.at(a, to))
-    end)
-    |> Enum.map(&List.first/1)
-  end
+  def part1(), do: move_boxes(&Enum.reverse/1)
+  def part2(), do: move_boxes(&(&1))
 end
 
 Day5.part1() |> IO.puts()
