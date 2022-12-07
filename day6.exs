5058f35b093493166539fb35ef62c73fa5ac70ee
Thomas Cowart <tncowart@gmail.com>
2022-12-06 22:08:45 -0500

    AoC day 6 fine I'll use sets

--- day6.exs
+++ day6.exs
@@ -1,25 +1,17 @@
 defmodule Day6 do
-  @input File.open!("day6.input", encoding: :utf8)
-         |> IO.read(:eof)
-         |> String.to_charlist()
+  @input File.open!("day6.input", encoding: :utf8) |> IO.read(:eof) |> String.to_charlist()
 
   def find_marker(size) do
     @input
-    |> Enum.chunk_every(size, 1)
-    |> Enum.with_index(size)
-    |> Enum.drop_while(fn {x, _} ->
-      size !=
-        Enum.sort(x)
-        |> Enum.dedup()
-        |> length()
-    end)
+    |> Stream.chunk_every(size, 1)
+    |> Stream.map(&MapSet.new/1)
+    |> Stream.with_index(size)
+    |> Stream.drop_while(fn {x, _} -> size != MapSet.size(x) end)
+    |> Enum.to_list()
     |> List.first()
     |> elem(1)
   end
-
-  def part1(), do: find_marker(4)
-  def part2(), do: find_marker(14)
 end
 
-Day6.part1() |> IO.puts()
-Day6.part2() |> IO.puts()
+Day6.find_marker(4) |> IO.puts()
+Day6.find_marker(14) |> IO.puts()
